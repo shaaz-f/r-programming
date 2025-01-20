aedf4e59ecee7225415023b56f4c711cc6b055f1
shaaz-f <shaaz.feera@gmail.com>
2025-01-19 20:16:36 -0700

    finished q1

--- a1/main.R
+++ a1/main.R
@@ -1,1 +1,12 @@
-## Assignment 1
+# Question 1, plotting Iris dataset
+plot(iris$Petal.Width, iris$Petal.Length, 
+     xlab = "Width", 
+     ylab = "Length",
+     pch = 16,
+     col = iris$Species)
+
+identify(iris$Petal.Width, iris$Petal.Length, labels = iris$Species)
+
+title("Iris Petal Width vs Iris Petal Length")
+legend("topleft", legend=c("Setosa", "Versicolor", "Virginica"), col=c(1,2,3), pch=19)
+
